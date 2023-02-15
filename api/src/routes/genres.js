984a626638a81721544b2a11e3cf56c9ffdc1785
Débora Mazzitelli <115846315+debora662@users.noreply.github.com>
2023-02-15 14:07:16 -0300

    Update genres.js

--- api/src/routes/genres.js
+++ api/src/routes/genres.js
@@ -5,7 +5,7 @@ const { getGenders } = require("../controllers/getGenders")
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    // llamamos al controlador, y segun nos responda respondemos el request. como hicimos en el CP de M3
+    // llamamos al controlador, y segun nos responda respondemos el request
     try {
         const response = await getGenders() 
         res.send(response)

d1f24937ffdec7084fc00c59ddda668a266049dd
Débora Mazzitelli <115846315+debora662@users.noreply.github.com>
2023-04-15 19:08:54 -0300

    change

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Detail from './components/Detail/Detail'
 import CreateVideogame from './components/CreateVideogame/CreateVideogame'
 import Nav from './components/Nav/Nav'
 import axios from 'axios'
-axios.defaults.baseURL = 'https://pi-videogames-kappa-lake.vercel.app/'
+axios.defaults.baseURL = 'https://pi-videogames-production-10be.up.railway.app/'
 
 function App () {
   
